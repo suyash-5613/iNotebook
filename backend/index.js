e58d1b830b57459b1739946e9de70cd2c894103a
suyash-5613 <suyashsahu0405@gmail.com>
2024-06-22 23:49:22 +0530

    basic setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const connectToMongo = require("./db");
+const express = require("express");
+const auth = require("./routes/auth.js");
+const notes = require("./routes/notes.js");
+
+connectToMongo();
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+app.use("/api/auth", auth);
+app.use("/api/notes", notes);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+ 
